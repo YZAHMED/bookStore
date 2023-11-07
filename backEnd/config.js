b40ae6019aca5013fc9ccd5d5c337164aac5c3ac
Jacobs <jacobs@Jacobss-MacBook-Pro.local>
2023-11-07 00:34:30 -0500

    Mongoose is installed and MongoDB is now connected

--- backEnd/config.js
+++ backEnd/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 
+"mongodb+srv://root:toor@bookstore.htzz1se.mongodb.net/bookStore?retryWrites=true&w=majority"
--- backEnd/index.js
+++ backEnd/index.js
@@ -1,8 +1,26 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModels.js";
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`App Listening on ${PORT}`);
-}); 
+app.get("/", (request, response) => {
+    return response.status(200).send("Server is Running");
+})
+
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log("mongoDB is now connected");
+
+        app.listen(PORT, () => {
+            console.log(`App Listening on ${PORT}`);
+        });
+
+
+    })
+    .catch((error) => {
+        console.log(error)
+    })
